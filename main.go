3b648415051b1e06be1611cbd507c7399a58fa3c
eligrealish <eli.grealish@gmail.com>
2024-02-20 21:19:08 +0000

    Notes from research

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ package main
 // TODO note down how to pull in dependencies "go get {$project_name} ."
 
 func main() {
-
+	//TODO below done you need to use structs, they encapslate data and passing data can be slow.
 	//TODO read this to understand how/why/when to write golang structs vs funcs. Possibly cross ref with C and C++ in chat gpt? https://chat.openai.com/c/0010bcc5-38c3-4186-ac35-6017ea1e0685
 	// Initialize Router
 	router.Initialize()
