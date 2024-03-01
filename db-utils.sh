878c7f21dcbd4d5e851ba7bbc2438bb5fb2544e9
eligrealish <eli.grealish@gmail.com>
2024-03-01 19:36:39 +0000

    Bug fixes

--- db-utils.sh
+++ db-utils.sh
@@ -23,10 +23,17 @@ case $operation in
     echo "Backup successful: $backup_file"
     ;;
   restore)
+    #backs up first
+    # Construct the backup file path
+    backup_file="$backup_dir/db_dump.$type.$datetime"
+    # Run mongodump to backup the database
+    sudo mongodump --db=$dbname --out=$backup_file
+    echo "Backup successful: $backup_file"
+
     # Construct the restore file path (assuming the restore file is named with the type but without a datetime)
     restore_file="./db_backup.$type/Night_Service"
     # Run mongorestore to restore the database
-    sudo mongorestore --db=$dbname $restore_file
+    sudo mongorestore --drop --db=$dbname $restore_file
     echo "Restore successful from: $restore_file"
     ;;
   *)
