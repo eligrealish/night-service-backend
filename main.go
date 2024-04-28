f18f76a89c7c336787934a470873d0b78d9b9734
eli <eli.grealish@gmail.com>
2024-04-28 21:17:05 +0100

    Added todos removed log statements

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
-	"log"
 	"night-service-backend/routes"
 )
 
 func main() {
-	//GET $DOMAIN/policy
-	//GET $DOMAIN/event/$id
-	//GET $DOMAIN/event?location=$location
-	//GET $DOMAIN/event?location=$location&startDate=$startDate&endDate=$endDate
-
-	log.Println("before router init")
+	// TODO general clean up
+	// TODO go through entire app and define way to remove repeated code e.g move to utils and other functions
+	// TODO add more comments in increase clarity
+	// TODO convert repo to run on Mac rather then WSL
+	// TODO create new DB data and convert DB utils to work on Mac
+	// TODO leave things like swagger till later
+	// TODO consider adding tests like unit,regression or integration test before front end or after front end development
+	// TODO consider if one or many test types should or should not be implemented
+	// TODO remove ugly log statements
+	// TODO review other TODOs
+	// TODO review if ./routes/router.go should be moved to ./main.go
+	// TODO review if error should be moved to model or utils
+	// TODO review if if else for 500 or 200 should be moved to utils
+	// TODO review logging can be improved
 
 	// Initialize Router
 	routes.Initialize()
-	log.Println("after router init")
 
 }
--- routes/router.go
+++ routes/router.go
@@ -2,25 +2,17 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
 )
 
 // Initializes and run the router.
 func Initialize() {
-	log.Println("init func")
-
 	router := gin.Default()
 
 	middleware := Middleware{}
 	router.Use(middleware.CheckPolicyAccepted())
-	log.Println("router struct created")
-
-	log.Println("before routes registered")
 	initializeRoutes(router)
-	log.Println("after routes registered")
 
 	err := router.Run()
-	log.Println("running router!")
 	if err != nil {
 		return
 	}
